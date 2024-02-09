e521afab15f2346e53b7dcb1d8c5a7696cb588b3
SidduPrabhu <116366539+SidduPrabhu@users.noreply.github.com>
2024-02-09 16:57:55 +0530

    Update Operation_Analytics.sql

--- Operation_Analytics.sql
+++ Operation_Analytics.sql
@@ -1,4 +1,64 @@
+#Question A:
+Calculate the number of jobs reviewed per hour per day for November 2020?
 
+#Number of Jobs Reviewed per Day:
+ 
+SELECT
+COUNT(job_id) / 30 num_of_jobs_reviewed_per_day
+FROM
+project1;
+
+#Number of Jobs Reviewed per Hour
+
+#Question B:
+Let’s say the above metric is called throughput. Calculate 7 day rolling average of throughput? For
+throughput, do you prefer daily metric or 7-day rolling and why?
+
+SELECT
+ b.*,round(avg(events_per_sec) OVER (order by date rows between 7 preceding and current
+row),2) 7_day_rolling_avg from
+(SELECT
+a.*,round(total_events/86400,2) events_per_sec
+ FROM
+(SELECT
+ DATE(occurred_at) date, COUNT(*) total_events
+ FROM
+ events
+GROUP BY DATE(occurred_at)) a)b;
+
+#Question C:
+Calculate the percentage share of each language in the last 30 days?
+
+SELECT
+ language,100*cnt/sum(cnt) OVER() percentage_shared
+FROM
+ (select *,count(language) cnt
+FROM
+ project1
+group by language) a
+
+#Question D:
+Let’s say you see some duplicate rows in the data. How will you display duplicates from the table?
+
+SELECT
+ *, COUNT(*) count
+FROM
+ project1
+GROUP BY ds , job_id , actor_id , event , language , time_spent , org
+HAVING COUNT(*) > 1;
+
+SELECT
+ (SELECT
+ COUNT(job_id) / 30 num_of_jobs_reviewed_per_day
+ FROM
+ project1) / 24 num_of_jobs_reviewed_per_hour
+FROM
+ project1;
+
+#Case Study - 2
+Question A:
+Calculate the weekly user engagement?
+    
 SELECT 
     a.*, COUNT(event_type) Total_engagement
 FROM
@@ -13,11 +73,17 @@ WHERE
     event_type = 'engagement'
 GROUP BY week;
 
+#Question B:
+Calculate the user growth for product?
+
 select a.*,count(event_type) Total_engagement from
 (select event_type,occurred_at,year(occurred_at),monthname(occurred_at) month,week(occurred_at) week from 
 events) a
 where event_type = "engagement"
 group by year(occurred_at),month(occurred_at),week(occurred_at);
+
+#Question C:
+Calculate the email engagement metrics?
 
 SELECT 
     YEAR(created_at) year,
@@ -33,6 +99,9 @@ FROM
     email_events
 GROUP BY action;
 
+#Question D:
+Calculate the weekly engagement per device?
+
 SELECT 
     device,
     WEEK(occurred_at) week,
@@ -43,6 +112,9 @@ WHERE
     event_type = 'engagement'
 GROUP BY device , WEEK(occurred_at)
 ORDER BY device , week;
+
+#Question E:
+Calculate the weekly retention of users-sign up cohort?
 
 SELECT 
     first_week,
@@ -147,10 +219,3 @@ FROM
 GROUP BY first_week
 ORDER BY first_week;
 
-SELECT 
-    language,100*cnt/sum(cnt) OVER() percentage_shared 
-FROM
-    (select *,count(language) cnt
-FROM
-     project1
-group by language) a;
